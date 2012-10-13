9ac85ecade960139adc9293dc476c40ecc308fe3
darthrake <darthrake@yasit.net>
2012-10-13 22:12:32 +0200

    adds spacecraft sample game, without accelerometer support

---
+++ firmware/spacecraft/src/Spacecraft.c
@@ -0,0 +1,188 @@
+#include <game/Game.h>
+#include <game/Debug.h>
+#include <game/Filesystem.h>
+
+#include <stdio.h>
+#include <string.h>
+#include <stm32f4xx/stm32f4xx_rng.h>
+
+#include "../inc/Sprites.h"
+
+static uint32_t sqrti(uint32_t n);
+
+void Init(void);
+void Update(uint32_t );
+void Draw(Bitmap* surface);
+
+Game MyGame = { Init, Update, Draw };
+Game* TheGame = &MyGame;
+
+#ifdef ACCELEROMETER
+static uint32_t sqrti(uint32_t n);
+static int8_t zero[3];
+static int32_t acc_x=0,acc_y=0;
+#endif
+
+int pos_x = (320-42)/2;
+int pos_y = (200-42)/2;
+int dir = 0;
+
+#define NumberOfStars 3
+static struct Star
+{
+	int x,y,dy,f;
+} stars[NumberOfStars];
+
+const RLEBitmap *sprites[7*6]={
+	&Star1_0,&Star2_0,&Star3_0,&Star4_0,&Star5_0,&Star6_0,&Star7_0,
+	&Star1_1,&Star2_1,&Star3_1,&Star4_1,&Star5_1,&Star6_1,&Star7_1,
+	&Star1_2,&Star2_2,&Star3_2,&Star4_2,&Star5_2,&Star6_2,&Star7_2,
+	&Star1_3,&Star2_3,&Star3_3,&Star4_3,&Star5_3,&Star6_3,&Star7_3,
+	&Star1_4,&Star2_4,&Star3_4,&Star4_4,&Star5_4,&Star6_4,&Star7_4,
+	&Star1_5,&Star2_5,&Star3_5,&Star4_5,&Star5_5,&Star6_5,&Star7_5,
+};
+
+const RLEBitmap *spacecraft[3]={
+	&Space_0_0, &Space_0_1, &Space_0_2
+};
+
+inline bool RectRectIntersection(int16_t x1, int16_t y1, int16_t w1, int16_t h1, int16_t x2, int16_t y2, int16_t w2, int16_t h2)
+{
+	return (x1 <= x2+w2 && x2 <= x1+w1 && y1 <= y2+h2 && y2 <= y1+h1);
+}
+
+void gameOver(Bitmap *currframe, int16_t pos_x, int16_t pos_y){
+	DrawFilledRectangle(currframe,0,0,320,200,RGB(255,0,0));
+}
+
+void Init()
+{
+	EnableDebugOutput(DEBUG_USART);
+	printf("Init\r\n");
+
+	InitializeLEDs();
+
+	SetLEDs(0x01);
+	SetLEDs(0x07);
+
+#ifdef ACCELEROMETER
+	InitializeAccelerometer();
+
+	SetAccelerometerMainConfig(
+		LIS302DL_LOWPOWERMODE_ACTIVE|
+		LIS302DL_DATARATE_100|
+		LIS302DL_XYZ_ENABLE|
+		LIS302DL_FULLSCALE_2_3|
+		LIS302DL_SELFTEST_NORMAL);
+
+	//Wait one second for data to stabilize
+	Delay(100);
+
+	SetAccelerometerFilterConfig(
+		LIS302DL_FILTEREDDATASELECTION_BYPASSED|
+		LIS302DL_HIGHPASSFILTER_LEVEL_1|
+		LIS302DL_HIGHPASSFILTERINTERRUPT_1_2);
+
+	Delay(50);
+	ReadRawAccelerometerData(zero);
+#endif
+
+	
+
+	for(int i=0;i<NumberOfStars;i++)
+	{
+		stars[i].x=GetRandomInteger()%360;
+		stars[i].y=(GetRandomInteger()%200);
+
+		int z=sqrti((NumberOfStars-1-i)*NumberOfStars)*1000/NumberOfStars;
+		stars[i].dy=1;//6000*1200/(z+200);
+
+		stars[i].f=(6-(z*7)/1000)+(GetRandomInteger()%6)*7;
+	} 
+}
+
+void Draw(Bitmap *surface) {
+		ClearBitmap(surface);
+
+		for(int i=0;i<NumberOfStars;i++)
+		{
+			DrawRLEBitmap(surface,sprites[stars[i].f],(stars[i].x),stars[i].y);
+
+			stars[i].y+=stars[i].dy;
+			if(stars[i].y>=(200))
+			{
+				stars[i].y=(0);
+				stars[i].x=GetRandomInteger()%320;
+				stars[i].f=(stars[i].f%7)+(GetRandomInteger()%6)*7;
+
+			}
+			const RLEBitmap sprite = *sprites[stars[i].f];
+			const RLEBitmap spaceBitmap = *spacecraft[0];
+			if (RectRectIntersection(stars[i].x, stars[i].y, sprite.width, sprite.height, pos_x, pos_y, spaceBitmap.width, spaceBitmap.height))
+			{
+				gameOver(surface, pos_x, pos_y);
+			}
+		}
+
+		DrawRLEBitmap(surface, spacecraft[dir+1], pos_x, pos_y);
+}
+
+void Update(uint32_t delta) {
+	#ifdef ACCELEROMETER
+			if (UserButtonState()) {
+				ReadRawAccelerometerData(zero);
+			}
+
+			int8_t components[3];
+			ReadRawAccelerometerData(components);
+			int32_t dx=components[1]-zero[1];
+			int32_t dy=components[0]-zero[0];
+
+			dx = (dx / 10) * 10;
+			dy = (dy / 10) * 10;
+
+			if (dx < 0) {
+				dir = 1;
+			} else if (dx > 0) {
+				dir = -1;
+			}
+
+			pos_x -= dx/10;
+			pos_y -= dy/10;
+
+			if (pos_x < 0) {
+				pos_x = 0;
+			}
+
+			if (pos_x >= 320) {
+				pos_x = 320;
+			}
+
+			if (pos_y >= 200) {
+				pos_y = 200;
+			}
+
+			if (pos_y < 0) {
+				pos_y = 0;
+			}
+	#endif
+}
+
+static uint32_t sqrti(uint32_t n)
+{
+	uint32_t s,t;
+
+	#define sqrtBit(k) \
+	t = s+(1UL<<(k-1)); t <<= k+1; if (n >= t) { n -= t; s |= 1UL<<k; }
+
+	s=0;
+	if(n>=1<<30) { n-=1<<30; s=1<<15; }
+	sqrtBit(14); sqrtBit(13); sqrtBit(12); sqrtBit(11); sqrtBit(10);
+	sqrtBit(9); sqrtBit(8); sqrtBit(7); sqrtBit(6); sqrtBit(5);
+	sqrtBit(4); sqrtBit(3); sqrtBit(2); sqrtBit(1);
+	if(n>s<<1) s|=1;
+
+	#undef sqrtBit
+
+	return s;
+}

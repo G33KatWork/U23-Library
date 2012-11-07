0ea3d1c81e4a8f212a30bbbab85464b5f1e2a682
Norbert Braun <n.braun@ikp.uni-koeln.de>
2012-11-08 00:13:47 +0100

    add serencode (for softscope)

---
+++ firmware/exposer/inc/serencode.h
@@ -0,0 +1,12 @@
+#ifndef _SERENCODE_H_
+#define _SERENCODE_H_
+
+#include <inttypes.h>
+
+void se_start_frame(uint8_t n);
+void se_putchr(char c);
+void se_puti16(uint16_t x);
+void se_puti32(uint32_t x);
+void se_putdata(const char* c, int n);
+
+#endif
